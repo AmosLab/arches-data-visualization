966d2ed5532b007ffd3d82e74e00649437f0de94
Alex Morain <amorain2@illinois.edu>
2021-03-23 16:41:34 -0500

    Working downloadable info on projects and investigators. Just need to add a few more details to each sheet

--- ARCHESgraph3.js
+++ ARCHESgraph3.js
@@ -179,6 +179,41 @@ d3.json(graphFile).then(function(graph) {
        }
    );
 
+   $('#download').on('click',
+       function exportCSV() {
+           console.log('hi')
+            rows = [['id','totalFunding']]
+            graph.nodes.forEach(function(d,i) {
+                rows.push([d.id,d.funding]);
+            });
+            let csvContent = "data:text/csv;charset=utf-8," 
+                + rows.map(e => e.join(",")).join("\n");
+            var encodedUri = encodeURI(csvContent);
+            var link = document.createElement("a");
+            link.setAttribute("href", encodedUri);
+            link.setAttribute("download", "investigators.csv");
+            document.body.appendChild(link); // Required for FF
+            link.click(); // This will download the data file named "my_data.csv".
+
+            rows = [['Project Name','Project Number', 'Year','Funding Amount','Investigators']]
+            projects = []
+            graph.links.forEach(function(l,i) {
+                if (!projects.includes(l.projectName)) {
+                    rows.push([l.projectName,l.projNum,l.year,l.amount,l.PIs + ", " + l.addInvestigators]);
+                    projects.push(l.projectName);
+                }
+            });
+            csvContent = "data:text/csv;charset=utf-8," 
+                + rows.map(e => e.join(",")).join("\n");
+            var encodedUri = encodeURI(csvContent);
+            var link = document.createElement("a");
+            link.setAttribute("href", encodedUri);
+            link.setAttribute("download", "projects.csv");
+            document.body.appendChild(link); // Required for FF
+            link.click(); // This will download the data file named "my_data.csv".
+       }
+   );
+
 
    $("#clear").click(
        function clearFocus() {
