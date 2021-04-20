39c328665fd996eb773b09b5eb137692ee7ddc76
Vincent Lam <78440218+vlam8@users.noreply.github.com>
2021-04-20 16:28:59 -0500

    Dynamic tag list is now functional

--- addTagList.js
+++ addTagList.js
@@ -6,19 +6,22 @@ d3.json(graphFile).then(function(graph) {
 
 function createTagList() {
 	graph.tagNames.forEach(function(key, value) {
-		// create div for new tag
+		// tag numbers start at 1, but JS array indexes start at 0
+		tagNumber = value+1;
+		
+		// create div for new tag, assign the ID to be the same as the tag name
 		const newTag = document.createElement('div');
-		newTag.setAttribute("id", String(value));
+		newTag.setAttribute("id", String(key[tagNumber]));
 		
 		// create a new checkbox element
 		const newCheckbox = document.createElement("INPUT");
 		newCheckbox.setAttribute("type", "checkbox");
-		newCheckbox.setAttribute("id", String(value));
+		newCheckbox.setAttribute("id", String(key[tagNumber]));
 		newTag.appendChild(newCheckbox);
 		
-		// and give it a label
+		// and give it a label with the tag name
 		const newLabel = document.createElement("Label");
-		newLabel.innerHTML = String(value);
+		newLabel.innerHTML = String(key[tagNumber]);
 		newTag.appendChild(newLabel);
 	
 		// add break
@@ -26,8 +29,7 @@ function createTagList() {
 		newTag.appendChild(br);
 		
 		// add div to tagList form
-		const tagListForm = document.getElementById("tagListWindow"); //need to add id tagList to form 
-		console.log(tagListForm);
+		const tagListForm = document.getElementById("tagListWindow"); //need to add id tagList to form
 		tagListForm.appendChild(newTag);
 	}
 )
